8e506cd4cdf657260b1420a63f91c05d558a4eaf
Nam Junghyun <13118114+cr0sh@users.noreply.github.com>
2023-06-04 11:13:21 +0900

    Add timestmap to logger

--- src/logger.ts
+++ src/logger.ts
@@ -2,6 +2,11 @@ import winston from "winston";
 
 export const logger = winston.createLogger({
     level: "info",
-    format: winston.format.simple(),
+    format: winston.format.combine(
+        winston.format.timestamp({
+            format: 'YYYY-MM-DD HH:mm:ss'
+        }),
+        winston.format.printf(info => `${info.timestamp} ${info.level}: ${info.message}` + (info.splat !== undefined ? `${info.splat}` : " "))
+    ),
     transports: [new winston.transports.Console()]
 })
